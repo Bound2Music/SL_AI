50df3c5baee9865d77aec14b22dd581ee58f7cc2
Bound2Music <johnb0521@hotmail.com>
2026-04-04 20:03:44 -0400

    Delete startup.sh

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Install Microsoft repo
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-# Install the ODBC driver
-apt-get update
-ACCEPT_EULA=Y apt-get install -y msodbcsql18
